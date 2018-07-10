ad115c3e97d05897bd35bd1717473d268d17c8aa
Fabian Dumke <fabian.dumke@student.hpi.uni-potsdam.de>
2018-07-10 02:15:23 +0200

    change pool feature setup

--- resnetfeats.py
+++ resnetfeats.py
@@ -9,9 +9,9 @@ pretrained_model = resnet101(pretrained=True)
 class Resnet101Feats(nn.Module):
   def __init__(self):
     super(Resnet101Feats, self).__init__()
-    self.features_nopool = nn.Sequential(*list(pretrained_model.children())[:-2])
+    self.features_nopool = nn.Sequential(*list(pretrained_model.children())[:-4])
     #self.features_pool = list(pretrained_model.children())[-3]
-    self.features_pool = list(pretrained_model.children())[-2]
+    self.features_pool = nn.Sequential(pretrained_model.children()[-4], pretrained_model.children()[-3], pretrained_model.children()[-2])
     self.classifier = nn.Sequential(list(pretrained_model.children())[-1]) # add one extra fc layer?
     #self.classifier = nn.Sequential(pretrained_mdoel.fc)
   def forward(self, x):
